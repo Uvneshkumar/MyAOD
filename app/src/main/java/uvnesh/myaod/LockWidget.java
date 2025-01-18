f8201fab5922c6ba397c64804e8a26ed48deea2a
Uvnesh <uvneshkumar@gmail.com>
2025-01-18 20:17:28 +0530

    Transparent widget to launch MyAOD

---
+++ app/src/main/java/uvnesh/myaod/LockWidget.java
@@ -0,0 +1,29 @@
+package uvnesh.myaod;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class LockWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.lock_widget);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                new Intent(context, MainActivity.class),
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+        views.setOnClickPendingIntent(R.id.lock, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
